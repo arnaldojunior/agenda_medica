368c1c5c85f8488506ba0b32e60e1d12022e2b44
Jeremias Neres <jeremiasnere23@gmail.com>
2019-01-30 21:16:16 -0300

    Adicionando camada de repositorios

---
+++ AgendaMedica/src/main/java/repositorio/MedicoRepositorio.java
@@ -0,0 +1,97 @@
+
+package repositorio;
+
+import javax.annotation.Resource;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.transaction.UserTransaction;
+
+
+
+/**
+ *
+ * @author root
+ */
+
+public class MedicoRepositorio {
+    
+    @PersistenceUnit(unitName = "")
+    EntityManagerFactory emf;
+    
+    @Resource
+    UserTransaction transaction;
+    
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+    public void create(Object medico) throws Exception {
+        
+        EntityManager em = null;
+        try {
+            transaction.begin();
+            em = getEntityManager();
+            em.persist(medico);
+            transaction.commit();
+        
+        } catch(Exception e) {
+            e.printStackTrace();
+            try {
+                transaction.rollback();
+            } catch (Exception ex) {
+                throw new Exception("Erro ao efetuar rollback: "+ ex);
+            }
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+     
+    public List<Object> findAll() {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            
+            return em.createQuery("SELECT z FROM Medico z").getResultList();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+     public void deletar(Long id) throws Exception {
+         EntityManager em = null;
+        try {
+            transaction.begin();
+            em = getEntityManager();
+            Object medico;
+            try {
+                medico = em.getReference(Object.class, id);
+            } catch (EntityNotFoundException enfe) {
+                throw new Exception("Medico com id " + id + " não existe.", enfe);
+            }
+            em.remove(medico);
+            transaction.commit();
+        
+        } catch(Exception e) {
+            e.printStackTrace();
+            try {
+                transaction.rollback();
+            } catch (Exception ex) {
+                throw new Exception("Erro ao efetuar rollback: "+ ex);
+            }
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+    
+    
+}
